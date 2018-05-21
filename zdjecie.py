6bc0ee24190e8ee2029007ceb2df21cfc82d6e71
Filip <glodekfilip@gmail.com>
2018-05-21 12:44:48 +0200

    wykrywanie twarzy

--- zdjecie.py
+++ zdjecie.py
@@ -1,22 +1,42 @@
-import urllib
+import urllib.request as urllib2
 import cv2
 import numpy as np
 import time
 
-url = 'http://192.168.43.1:8888/shot.jpg'
+# url = 'http://192.168.0.220:8080/shot.jpg'
+# import os
+#
+# def createFolder(directory):
+#     try:
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
+#     except OSError:
+#         print('Error: Creating directory. ' + directory)
+#
+# # Example
+# createFolder('./data/')
 
-while True:
 
-    # Use urllib to get the image and convert into a cv2 usable format
-    imgResp = urllib.urlopen(url)
-    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
-    img = cv2.imdecode(imgNp, -1)
+# while True:
 
-    # put the image on screen
-    cv2.imshow('IPWebcam', img)
+# Use urllib2 to get the image and convert into a cv2 usable format
+# imgResp = urllib2.urlopen(url)
+# imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+# img = cv2.imdecode(imgNp, -1)
 
-    # To give the processor some less stress
-    # time.sleep(0.1)
+# put the image on screen
+# cv2.imshow('MyIPWebcam', img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# To give the processor some less stress
+# time.sleep(0.1)
+
+
+# if cv2.waitKey(1) & 0xFF == ord('q'):
+#     cv2.imwrite('asdf221.png', img)
+#     break
+
+from time import time, sleep
+
+print(int(time()))
+sleep(2)
+print(int(time()))
